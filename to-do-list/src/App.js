ca8302138bf163b68b1cb444c730381ac5465d74
BSimardJulien <julien.b.simard@outlook.com>
2021-01-08 15:06:00 -0500

    wow

---
+++ to-do-list/src/App.js
@@ -0,0 +1,84 @@
+import { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newItem: "",
+      list: []
+    }
+  }
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list];
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div id ="flexmiddle">
+          <div id="insideApp">
+            Add an item...
+        <br />
+            <input
+              type="text"
+              placeholder="Type item here..."
+              value={this.state.newItem}
+              onChange={e => this.updateInput("newItem", e.target.value)}
+            />
+            <button
+              onClick={() => this.addItem()}
+            >
+              addItem
+        </button>
+            <br />
+            <ul id="added-elements">
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id}>
+                    {item.value}
+                    <button
+                      onClick={() => this.deleteItem(item.id)}
+                    >
+                      X
+                </button>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
